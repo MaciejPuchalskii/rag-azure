a2a4d5d0bcf37d542760251d999bf1f489f45d40
maciej.a.puchalski@avanade.com <maciej.a.puchalski@avanade.com>
2026-07-17 11:59:23 +0200

    project init

---
+++ ingest.py
@@ -0,0 +1,6 @@
+# 1. Odczytaj PDF
+# 2. Wyciągnij tekst
+# 3. Podziel na chunki
+# 4. Dla każdego chunka:
+#       wygeneruj embedding
+#       zapisz do AI Search
---
+++ test_embedding.py
@@ -0,0 +1,18 @@
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+response = client.embeddings.create(
+    model=os.getenv("EMBEDDING_DEPLOYMENT"),
+    input="What is Delta Lake?"
+)
+
+print(len(response.data[0].embedding))
